e68fc23f40e40313c273aeefc1e5b493127adf02
smishra11 <msubhasish11@gmail.com>
2020-12-15 00:19:24 +0530

    receiving data faker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Modal from './Modal';
+import faker from 'faker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    persons: [],
+    isClicked: false,
+    userDetails: {},
+    isDetailsClicked: false,
+  };
+
+  generateData = () => {
+    let arr = [];
+    for (let i = 1; i <= 5; i++) {
+      let obj = {
+        name: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          gender: faker.name.gender(),
+          email: faker.internet.email(),
+          phone: faker.phone.phoneNumber(),
+        },
+        address: {
+          streetName: faker.address.streetName(),
+          state: faker.address.state(),
+          city: faker.address.city(),
+          zip: faker.address.zipCode(),
+          country: faker.address.country(),
+          timeZone: faker.address.timeZone(),
+        },
+        vehicle: {
+          model: faker.vehicle.model(),
+          color: faker.vehicle.color(),
+          manufacturer: faker.vehicle.manufacturer(),
+        },
+        company: {
+          name: faker.company.companyName(),
+          jobTitle: faker.name.jobTitle(),
+          jobArea: faker.name.jobArea(),
+        },
+        finance: {
+          accountName: faker.finance.accountName(),
+          accountNumber: faker.random.number({
+            min: 100000000000,
+            max: 999999999999,
+          }),
+          amount: faker.finance.amount(),
+          currencySymbol: faker.finance.currencySymbol(),
+          creditCardNumber: faker.finance.creditCardNumber(),
+          creditCardCVV: faker.finance.creditCardCVV(),
+        },
+      };
+      arr.push(obj);
+    }
+    this.setState({ persons: arr });
+  };
+
+  componentDidMount() {
+    this.generateData();
+  }
+
+  deatilsClicked = (p) => {
+    this.setState({ userDetails: p, isDetailsClicked: true });
+    console.log(p, '--clicked');
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <div>
+          <button
+            className="btn btn-success"
+            type="button"
+            data-toggle="modal"
+            data-target="#exampleModal"
+            onClick={this.clicked}
+          >
+            Button
+          </button>
+          {this.state.isClicked ? <Modal /> : null}
+        </div> */}
+        <h2
+          style={{
+            marginTop: '1rem',
+            textAlign: 'center',
+            color: 'deepskyblue',
+            fontWeight: 'bold',
+            textDecoration: 'underline dotted grey',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          React with Faker
+        </h2>
+        <div>
+          {this.state.persons.map((p, i) => {
+            return (
+              <div
+                style={{ display: 'flex', justifyContent: 'center' }}
+                key={i}
+              >
+                <div className="card mt-3 bg-light" style={{ width: '50%' }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className="card-body" style={{ fontWeight: 'bold' }}>
+                      {p.name.firstName} {p.name.lastName}
+                    </div>
+                    <div
+                      style={{
+                        color: 'deepskyblue',
+                        textDecoration: 'underline',
+                        marginRight: '20px',
+                        cursor: 'pointer',
+                      }}
+                      data-toggle="modal"
+                      data-target="#exampleModal"
+                      onClick={() => this.deatilsClicked(p)}
+                    >
+                      View details
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {this.state.isDetailsClicked ? (
+          <Modal userDetails={this.state.userDetails} />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+  render() {
+    const { userDetails } = this.props;
+    return (
+      <div
+        className="modal fade bd-example-modal-lg"
+        id="exampleModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">{userDetails.name.firstName}</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
